a639530b1ccff8d6e63489bff84dc426b3c71def
Asfarus1 <Asfarus1@gmail.com>
2017-01-15 23:20:47 +1000

    meal repository

--- src/main/java/ru/javawebinar/topjava/repository/datajpa/CrudMealRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/datajpa/CrudMealRepository.java
@@ -1,11 +1,35 @@
 package ru.javawebinar.topjava.repository.datajpa;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import ru.javawebinar.topjava.model.Meal;
+import ru.javawebinar.topjava.model.User;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 /**
  * gkislin
  * 02.10.2016
  */
 public interface CrudMealRepository extends JpaRepository<Meal, Integer> {
+    List<Meal> findByUserOrderByDateTime(User user);
+
+    @Override
+    void delete(Meal meal);
+
+    @Override
+    Meal save(Meal meal);
+
+    @Modifying
+    @Query("SELECT m FROM Meal m WHERE u.id:=id AND u.user.id=:userId")
+    Meal findOne(@Param("id") int id,@Param("userId") int userId);
+
+    List<Meal> findByUserAndDateTimeBetweenOrderByDateTime(User user, LocalDateTime startDate, LocalDateTime endDate);
+
+    @Modifying
+    @Query("DELETE FROM Meal m WHERE u.id=:id AND u.user.id=:userId")
+    int delete(@Param("id") int id,@Param("userId") int userId);
 }
--- src/main/java/ru/javawebinar/topjava/repository/datajpa/DataJpaMealRepositoryImpl.java
+++ src/main/java/ru/javawebinar/topjava/repository/datajpa/DataJpaMealRepositoryImpl.java
@@ -2,9 +2,13 @@ package ru.javawebinar.topjava.repository.datajpa;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 import ru.javawebinar.topjava.model.Meal;
+import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.repository.MealRepository;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import java.time.LocalDateTime;
 import java.util.List;
 
@@ -19,18 +23,26 @@ public class DataJpaMealRepositoryImpl implements MealRepository {
     private CrudMealRepository crudRepository;
 
     @Override
-    public Meal save(Meal Meal, int userId) {
+    @Transactional
+    public Meal save(Meal meal, int userId) {
+        if (meal.isNew()){
+            return crudRepository.save(meal);
+        }else {
+            if (crudRepository.findOne(meal.getId(),userId)!=null){
+                return crudRepository.save(meal);
+            }
+        }
         return null;
     }
 
     @Override
     public boolean delete(int id, int userId) {
-        return false;
+        return crudRepository.delete(id,userId)!=0;
     }
 
     @Override
     public Meal get(int id, int userId) {
-        return null;
+        return crudRepository.findOne(id, userId);
     }
 
     @Override
